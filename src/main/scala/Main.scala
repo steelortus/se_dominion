7ab8fb86e335136a2fb1c54ff3597bb4099447c4
steelortus <3wa.wf2e@gmx.de>
2025-04-10 12:50:24 +0200

    added enum 'Card' and added some cards to it

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,17 @@
 import io.StdIn._
 import scala.collection.mutable.ListBuffer
+import de.htwg.se.dominion.Card
+
+
 val cards = List(("Kupfer", 0, 1, 0, 50), ("Silber", 3, 2, 0, 50), ("Gold", 6, 3, 0, 50), ("Anwesen", 2, 0, 1, 50),
                   ("Herzogtum", 5, 0, 3, 25), ("Provinz", 8, 0, 6, 12), ("Fluch", 0, 0, -1, 50), ("Burggraben", 2, 0, 0, 10),
-                  ("Miliz", 4, 0, 0, 9), ("Holzfaeller", 3, 0, 0, 10), ("Markt", 5, 0, 0, 10), ("Laborant", 4, 0, 0, 10),
-                  ("Wachstube", 4, 0, 0, 10), ("Keller", 2, -1, -1, -1), ("Garten", -1, -1, -1, -1))
+                  ("Miliz", 4, 0, 0, 9), ("Holzfaeller", 3, 0, 0, 10), ("Markt", 5, 0, 0, 10), ("Laboratorium", 4, 0, 0, 10),
+                  ("Keller", 2, -1, -1, -1), ("Garten", -1, -1, -1, -1))
 val stock = ListBuffer[(String, Int, Int, Int, Int)]()
 
 
 @main def main(): Unit = {
+  println(Card.Kupfer)
   println("Please type out a Card you want to add to the Stock:")
   if (addCard(getCard(readLine()))) {
     println("Card added successfully.")
