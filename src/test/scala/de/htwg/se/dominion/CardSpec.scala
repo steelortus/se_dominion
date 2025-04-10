e1e5512dfe155e189b74ec73f15e5a31fee2342c
steelortus <3wa.wf2e@gmx.de>
2025-04-10 13:54:23 +0200

    added more cases to CardSpec

--- src/test/scala/de/htwg/se/dominion/CardSpec.scala
+++ src/test/scala/de/htwg/se/dominion/CardSpec.scala
@@ -5,6 +5,8 @@ import scalatest.wordspec.AnyWordSpec
 
 class CardSpec extends AnyWordSpec {
     "A Card" should {
+        val s = new Stock()
+        s.setupStock()
         val card = Card.Kupfer
         "have a getName" in {
             card.getName should be("Kupfer")
@@ -20,6 +22,54 @@ class CardSpec extends AnyWordSpec {
         }
         "have a getAmount" in {
             card.getAmount should be(50)
+        }
+        "have an auto setup Stock" in {
+            s.stock.length should be(7)
+            s.stock.contains(Card.Kupfer) should be(true)
+            s.stock.contains(Card.Silber) should be(true)
+            s.stock.contains(Card.Gold) should be(true)
+            s.stock.contains(Card.Anwesen) should be(true)
+            s.stock.contains(Card.Herzogtum) should be(true)
+            s.stock.contains(Card.Provinz) should be(true)
+            s.stock.contains(Card.Fluch) should be(true)
+
+            s.stock.contains(Card.Holzfaeller) should be(false)
+            s.stock.contains(Card.Miliz) should be(false)
+        }
+        "add a Card to the Stock" in {
+            s.addCard(Card.Holzfaeller) should be(true)
+            s.stock.contains(Card.Holzfaeller) should be(true)
+            s.stock.length should be(8)
+
+            s.AddCard(Card.Miliz) should be(true)
+            s.stock.contains(Card.Miliz) should be(true)
+            s.stock.length should be(9)
+        }
+        "not add a Card to the Stock if it already exists in the Stock" in {
+            s.addCard(Card.Kupfer) should be(false)
+            s.stock.length should be(9)
+            
+            s.addCard(Card.Silber) should be(false)
+            s.stock.length should be(9)
+
+            s.addCard(Card.Gold) should be(false)
+            s.stock.length should be(9)
+        }
+        "not add a Card to the Stock if it exceeds the limit" in {
+            s.addCard(Card.Garten) should be(true)
+            s.addCard(Card.Markt) should be(true)
+            s.addCard(Card.Jahrmarkt) should be(true)
+            s.addCard(Card.Laboratorium) should be(true)
+            s.addCard(Card.Burggraben) should be(true)
+            s.addCard(Card.Kappele) should be(true)
+            s.addCard(Card.Keller) should be(true)
+            s.addCard(Card.Bibliothek) should be(true)
+            s.stock.length should be(17)
+
+            s.addCard(Card.Abenteurer) should be(false)
+            s.stock.length should be(17)
+            s.addCard(Card.Dieb) should be(false)
+            s.stock.length should be(17)
         }
     }
 }
