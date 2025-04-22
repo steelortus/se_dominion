d8edb076e33d5616d518a03a1e88d4e79bed85fb
steelortus <3wa.wf2e@gmx.de>
2025-04-22 10:06:53 +0200

    added (unimplemented) play option to TUI

--- src/main/scala/de/htwg/se/dominion/TUI.scala
+++ src/main/scala/de/htwg/se/dominion/TUI.scala
@@ -17,6 +17,8 @@ case class TUI() {
                     add(stock)
                 case "remove" =>
                     remove(stock)
+                case "play" =>
+                    play(stock)
                 case "show" =>
                     println(stock.toString())
                     stock // Return the unchanged stock
@@ -28,7 +30,7 @@ case class TUI() {
         }
     }
 
-    def add(stock: Stock): Stock = {
+    def add(stock:Stock): Stock = {
         println("Please type out a Card you want to add to the Stock:")
         print("> ")
         val cardName = readLine()
@@ -37,12 +39,24 @@ case class TUI() {
         updatedStock
     }
 
-    def remove(stock: Stock): Stock = {
+    def remove(stock:Stock): Stock = {
         println("Please type out a Card you want to remove from the Stock:")
         print("> ")
         val cardName = readLine()
         val updatedStock = stock.removeCard(stock.getCard(cardName))
         println(updatedStock.toString())
         updatedStock
+    }
+
+    def play(stock:Stock): Stock = {
+        if (stock.getLength() < 17) {
+            println(s"There is not enough cards in the Stock! ${stock.getLength()} out of required 17")
+            stock // Return the unchanged stock
+        } else {
+            println("Playing the game... (not implemented yet)")
+            println(s"There are ${stock.getLength()} cards in the Stock.")
+            stock // Return the unchanged stock
+
+        }
     }
 }
