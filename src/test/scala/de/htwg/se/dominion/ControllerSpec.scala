8a0652093ba2f4265050a995740982239b326f5e
steelortus <3wa.wf2e@gmx.de>
2025-05-12 17:11:35 +0200

    added State Pattern

--- src/test/scala/de/htwg/se/dominion/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/dominion/ControllerSpec.scala
@@ -12,85 +12,45 @@ class ControllerSpec extends AnyWordSpec {
         val initialStock = new Stock
         val testCard = Card.Markt
 
-        "add a card to the stock if it is not full and the card is not already present" in {
+        "start in Preparation State" in {
             val controller = Controller(initialStock)
-
-            val result = controller.addCard(testCard.toString())
-
-            result should include ("Card added successfully")
-            controller.getStock().contains(testCard) should be(true)
+            controller.state should be(StatePreparation(controller, initialStock))
         }
 
-        "not add a card if it is already in stock" in {
-            val controller = Controller(initialStock)
-            controller.addCard(testCard.toString())
-
-            val result = controller.addCard(testCard.toString())
-
-            result should include ("Maybe it's already in it")
-            controller.getStock().getLength() should be(8)
-        }
-
-        "not add a card if stock is full" in {
-            val controller = Controller(initialStock)
-            val fullStock = controller.fillStock()
-        
-            val result = controller.addCard("Garten")
-
-            result should include ("Stock is already full")
-            controller.getStock().getLength() should be(17)
-        }
-
-        "notify observers with stockFull event when stock reaches 17 cards" in {
+        "switch to Playing State when play is called if Stock is full" in {
             val controller = Controller(initialStock)
 
-            var receivedEvent: Option[Event] = None
-            val testObserver = new Observer {
-                override def update(e: Event): Unit = receivedEvent = Some(e)
-            }
-            controller.add(testObserver)
-            
-            val cards = List(
-                Card.Burggraben, Card.Kapelle, Card.Keller, Card.Dorf, Card.Holzfaeller,
-                Card.Werkstatt, Card.Buerokrat, Card.Dieb, Card.Festmahl, Card.Geldverleiher
-            )
+            controller.play()
+            controller.state should be(StatePreparation(controller, initialStock))
 
-            cards.foreach(card => controller.addCard(card.toString()))
-
-            receivedEvent should be(Some(Event.stockFull))
+            controller.fillStock()
+            controller.play()
+            controller.state should be(StatePlaying(controller, controller.getStock()))
         }
 
-        "remove a card that is in stock" in {
+        "return it's associated stock" in {
+            val controller = Controller(initialStock)
+            controller.getStock() should be(initialStock)
+        }
+
+        "return a list of all Cards not in it's Stock in Preparation State" in {
+            val controller = Controller(initialStock)
+            val regx = "Liste der noch verfuegbaren Karten:\n\n([a-zA-Z]* \\| )*[a-zA-Z]*"
+            controller.listCards() should fullyMatch regex regx
+        }
+
+        "return of a list of all Cards in it's Stock in Playing State" in {
             val controller = Controller(initialStock)
             controller.fillStock()
-
-            val result = controller.removeCard(testCard.toString())
-
-            result should include ("Card removed successfully")
-            controller.getStock().contains(testCard) should be(false)
+            controller.play()
+            val regx = "([a-zA-Z]+ - Cost: [0-8]{1}, Value: [0123]{1}, Points: (-1|[01369])+, Amount: [0-9]+\n?)*"
+            controller.listCards() should fullyMatch regex regx
         }
 
-        "not remove a card that is not in stock" in {
+        "add a Card to the Stock in Preparation State" in {
             val controller = Controller(initialStock)
-
-            val result = controller.removeCard(testCard.toString())
-
-            result should include ("Maybe it's not in it")
-        }
-
-        "fill stock with predefined cards" in {
-            val controller = Controller(initialStock)
-            controller.fillStock()
-
-            controller.getStock().getLength() should be > 0
-            controller.getStock().getLength() should be(17)
-            controller.getStock().getCard("Holzfaeller") should be(Card.Holzfaeller)
-        }
-
-        "list cards that are not in the stock" in {
-            val controller = Controller(initialStock)
-
-            noException should be thrownBy controller.listCards()
+            val newStock = controller.state.addCard(testCard.toString, initialStock)
+            newStock.contains(testCard) should be(true)
         }
     }
 }
