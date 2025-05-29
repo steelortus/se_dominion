8bf7d5e357f459bd91b00eb8ff34f007e40a1e65
steelortus <3wa.wf2e@gmx.de>
2025-05-29 16:17:46 +0200

    test... test... is this thing on?

---
+++ src/test/scala/de/htwg/se/dominion/model/CardSpec.scala
@@ -0,0 +1,206 @@
+package de.htwg.se.dominion.model
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.dominion.model.Card
+
+class CardSpec extends AnyWordSpec {
+    "A Card" should {
+        val card = Card.Kupfer
+        "have a getName" in {
+            card.getName should be("Kupfer")
+            card.getName should not be("Peter")
+            card.getName should not be("NotACard")
+        }
+        "have a getCost" in {
+            card.getCost should be(0)
+            card.getCost should not be(1)
+            card.getCost should not be(-1)
+        }
+        "have a getValue" in {
+            card.getValue should be(1)
+            card.getValue should not be(0)
+            card.getValue should not be(-1)
+        }
+        "have a getPoints" in {
+            card.getPoints should be(0)
+            card.getPoints should not be(1)
+            card.getPoints should not be(-1)
+        }
+        "have a getAmount" in {
+            card.getAmount should be(50)
+            card.getAmount should not be(0)
+            card.getAmount should not be(100)
+        }
+    }
+    "A Stock" should {
+        var s = new Stock()
+        val regx = "([a-zA-Z]+ - Cost: [0-8]{1}, Value: [0123]{1}, Points: (-1|[01369])+, Amount: [0-9]+\n?)+"
+        "have an auto setup Stock" in {
+            s.stock.length should be(7)
+            s.stock.contains(Card.Kupfer) should be(true)
+            s.stock.contains(Card.Silber) should be(true)
+            s.stock.contains(Card.Gold) should be(true)
+            s.stock.contains(Card.Anwesen) should be(true)
+            s.stock.contains(Card.Herzogtum) should be(true)
+            s.stock.contains(Card.Provinz) should be(true)
+            s.stock.contains(Card.Fluch) should be(true)
+
+            s.toString should fullyMatch regex regx
+
+            s.stock.contains(Card.Holzfaeller) should be(false)
+            s.stock.contains(Card.Miliz) should be(false)
+        }
+        "add a Card to the Stock" in {
+            s = s.addCard(Card.Holzfaeller)
+            s.stock.contains(Card.Holzfaeller) should be(true)
+            s.getLength() should be(8)
+            s.toString should fullyMatch regex regx
+
+            s = s.addCard(Card.Miliz)
+            s.stock.contains(Card.Miliz) should be(true)
+            s.getLength() should be(9)
+        }
+        "not add a Card to the Stock if it already exists in the Stock" in {
+            val regularStock = new Stock()
+            var updatedStock = new Stock()
+
+            updatedStock = updatedStock.addCard(Card.Kupfer)
+            updatedStock == regularStock should be(true)
+            updatedStock.stock.length should be(7)
+            
+            updatedStock = updatedStock.addCard(Card.Silber)
+            updatedStock == regularStock should be(true)
+            updatedStock.stock.length should be(7)
+
+            updatedStock = updatedStock.addCard(Card.Gold)
+            updatedStock == regularStock should be(true)
+            updatedStock.stock.length should be(7)
+
+            updatedStock.toString should fullyMatch regex regx
+        }
+
+        "not add a Card to the Stock if it exceeds the limit" in {
+            s = s.addCard(Card.Garten)
+            s.stock.contains(Card.Garten) should be(true)
+
+            s = s.addCard(Card.Markt)
+            s = s.addCard(Card.Jahrmarkt)
+            s = s.addCard(Card.Laboratorium)
+            s = s.addCard(Card.Burggraben)
+            s = s.addCard(Card.Kapelle)
+            s = s.addCard(Card.Keller)
+            s = s.addCard(Card.Bibliothek)
+            s.stock.length should be(17)
+
+            s.toString should fullyMatch regex regx
+
+            var updatedStock = s
+            updatedStock = updatedStock.addCard(Card.Abenteurer)
+            updatedStock == s should be(true)
+            updatedStock.stock.length should be(17)
+            updatedStock.toString should fullyMatch regex regx
+        }
+
+        "addCard with String input" in {
+            var filledStock = new Stock()
+            filledStock = filledStock.addCard("Dorf")
+            filledStock.stock.contains(Card.Dorf) should be(true)
+            filledStock.getLength() should be (8)
+
+            var invalidStock = filledStock.addCard("Peter")
+            invalidStock == filledStock should be(true)
+            invalidStock.getLength() should be (8)
+
+            filledStock = filledStock.addCard("Garten")
+            filledStock = filledStock.addCard("Markt")
+            filledStock = filledStock.addCard("jahrmarkt")
+            filledStock = filledStock.addCard("LABORAtorium")
+            filledStock = filledStock.addCard("burggraben")
+            filledStock = filledStock.addCard("Kapelle")
+            filledStock = filledStock.addCard("Keller")
+            filledStock = filledStock.addCard("Bibliothek")
+            filledStock = filledStock.addCard("Miliz")
+            filledStock.stock.length should be(17)
+
+            invalidStock = filledStock.addCard("Abenteurer")
+            invalidStock == filledStock should be(true)
+            invalidStock.stock.length should be(17)
+
+            filledStock.toString should fullyMatch regex regx
+        }
+        "getCard returns the Card or NotACard" in {
+            s.getCard("Kupfer") should be(Card.Kupfer)
+            s.getCard("Silber") should be(Card.Silber)
+            s.getCard("Peter") should be(Card.NotACard)
+            s.getCard("NotACard") should be(Card.NotACard)
+            s.getCard("Garten") should be(Card.Garten)
+            s.getCard("Markt") should be(Card.Markt)
+        }
+        "cards should be in Stock" in {
+            s.stock should contain (Card.Garten)
+            s.stock should contain (Card.Markt)
+            s.stock should contain (Card.Jahrmarkt)
+            s.stock.contains(Card.Dieb) should be(false)
+            s.stock.contains(Card.Abenteurer) should be(false)
+        }
+        "remove a Card from the Stock" in {
+            s = s.removeCard(Card.Holzfaeller)
+            s.stock.contains(Card.Holzfaeller) should be(false)
+            s.stock.length should be(16)
+
+            s = s.removeCard("Miliz")
+            s.stock.contains(Card.Miliz) should be(false)
+            s.stock.length should be(15)
+        }
+        "not remove a Card from the Stock if it's mandatory" in {
+            val originalStock = s
+
+            s = s.removeCard(Card.Kupfer)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Silber)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Gold)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Anwesen)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Herzogtum)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Provinz)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s = s.removeCard(Card.Fluch)
+            s == originalStock should be(true)
+            s.stock.length should be(15)
+
+            s.toString should fullyMatch regex regx
+        }
+        "not remove a Card if it's not in the Stock" in {
+            s = s.removeCard(Card.Holzfaeller)
+            s.stock.length should be(15)
+
+            s = s.removeCard("Miliz")
+            s.stock.length should be(15)
+
+            s.toString should fullyMatch regex regx
+        }
+        "have a method to check, if a card is contained in the stock" in {
+            s = new Stock()
+            s.contains("Kupfer") should be(true)
+            s.contains("Anwesen") should be(true)
+            s.contains("Gold") should be(true)
+            s.contains("Dorf") should be(false)
+        }
+    }
+}
---
+++ src/test/scala/de/htwg/se/dominion/model/PlayerSpec.scala
@@ -0,0 +1,229 @@
+package de.htwg.se.dominion.model
+
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import de.htwg.se.dominion.model.Card
+import de.htwg.se.dominion.model.Player
+import de.htwg.se.dominion.model.Stock
+
+class PlayerSpec extends AnyWordSpec {
+    "A Player" should {
+        var debug_stock = Stock()
+        debug_stock = debug_stock.addCard(Card.Garten)
+        debug_stock = debug_stock.addCard(Card.Markt)
+        debug_stock = debug_stock.addCard(Card.Jahrmarkt)
+        debug_stock = debug_stock.addCard(Card.Dieb)
+        debug_stock = debug_stock.addCard(Card.Abenteurer)
+        debug_stock = debug_stock.addCard(Card.Dorf)
+        debug_stock = debug_stock.addCard(Card.Hexe)
+        debug_stock = debug_stock.addCard(Card.Laboratorium)
+        debug_stock = debug_stock.addCard(Card.Bibliothek)
+        val stock = debug_stock.addCard(Card.Holzfaeller)
+
+        val player = Player()
+        val regx = "(((Deck|Hand|Discard):\n?)?([a-zA-Z]+ - Value: [0123]{1}, Points: (-1|[01369])+\n?)*\n?)+"
+        "have a deck" in {
+            player.deck.length should be(10)
+
+            player.deck.contains(Card.Kupfer) should be(true)
+            player.deck.contains(Card.Anwesen) should be(true)
+            player.deck.contains(Card.Herzogtum) should be(false)
+            player.deck.contains(Card.Provinz) should be(false)
+
+            player.deckToString() should fullyMatch regex regx
+        }
+        "have a hand" in {
+            player.hand.length should be(0)
+
+            player.hand.contains(Card.Kupfer) should be(false)
+            player.hand.contains(Card.Anwesen) should be(false)
+            player.hand.contains(Card.Herzogtum) should be(false)
+            player.hand.contains(Card.Provinz) should be(false)
+
+            player.handToString() should fullyMatch regex regx
+        }
+        "have a discard" in {
+            player.discard.length should be(0)
+
+            player.discard.contains(Card.Kupfer) should be(false)
+            player.discard.contains(Card.Anwesen) should be(false)
+            player.discard.contains(Card.Herzogtum) should be(false)
+            player.discard.contains(Card.Provinz) should be(false)
+
+            player.discardToString() should fullyMatch regex regx
+        }
+        "shuffle the deck" in {
+            val shuffledPlayer = player.shuffleDeck()
+
+            shuffledPlayer.deck.length should be(10)
+            shuffledPlayer.deck should not be player.deck
+
+            shuffledPlayer.deckToString() should fullyMatch regex regx
+        }
+        "draw a card from the deck" in {
+            val drawnPlayer = player.drawCardFromDeck()
+
+            drawnPlayer.deck.length should be(9)
+            drawnPlayer.hand.length should be(1)
+
+            player.deckToString() should fullyMatch regex regx
+            drawnPlayer.handToString() should fullyMatch regex regx
+        }
+        "discard a card from the deck" in {
+            val discardedPlayer = player.discardFromDeck(0)
+
+            discardedPlayer.deck.length should be(9)
+            discardedPlayer.discard.length should be(1)
+
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.discardToString() should fullyMatch regex regx
+        }
+
+        "discard a specific card from the deck" in {
+            val discardedPlayer = player.discardFromDeck(8)
+
+            discardedPlayer.deck.length should be(9)
+            discardedPlayer.discard.length should be(1)
+
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.discardToString ()should fullyMatch regex regx
+        }
+        
+        "not discard a Card that is outside of the Deck" in {
+            val discardedPlayer = player.discardFromDeck(10)
+
+            discardedPlayer.deck.length should be(10)
+            discardedPlayer.discard.length should be(0)
+
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.discardToString() should fullyMatch regex regx
+        }
+
+        "discard a card from the hand" in {
+            val drawnPlayer = player.drawCardFromDeck()
+            val discardedPlayer = drawnPlayer.discardFromHand(0)
+
+            discardedPlayer.deck.length should be(9)
+            discardedPlayer.hand.length should be(0)
+            discardedPlayer.discard.length should be(1)
+            
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.handToString() should fullyMatch regex regx
+            discardedPlayer.discardToString() should fullyMatch regex regx
+        }
+
+        "discard a specific card from the hand" in {
+            var drawnPlayer = player.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+
+            drawnPlayer.hand.length should be(5)
+            drawnPlayer.deck.length should be(5)
+
+            val discardedPlayer = drawnPlayer.discardFromHand(2)
+            discardedPlayer.deck.length should be(5)
+            discardedPlayer.hand.length should be(4)
+            discardedPlayer.discard.length should be(1)
+
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.handToString() should fullyMatch regex regx
+            discardedPlayer.discardToString() should fullyMatch regex regx
+        }
+
+        "not discard a Card that is outside of the Hand" in {
+            var drawnPlayer = player.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+
+            drawnPlayer.hand.length should be(5)
+            drawnPlayer.deck.length should be(5)
+
+            val discardedPlayer = drawnPlayer.discardFromHand(10)
+            discardedPlayer.deck.length should be(5)
+            discardedPlayer.hand.length should be(5)
+            discardedPlayer.discard.length should be(0)
+
+            discardedPlayer.deckToString() should fullyMatch regex regx
+            discardedPlayer.handToString() should fullyMatch regex regx
+            discardedPlayer.discardToString() should fullyMatch regex regx
+        }
+
+        "refill the Deck if it's empty" in {
+            var drawnPlayer = player.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+            drawnPlayer = drawnPlayer.drawCardFromDeck()
+
+            val emptyDeckPlayer = drawnPlayer.discardFromHand(0)
+
+            emptyDeckPlayer.deck.length should be(0)
+
+            val refilledPlayer = emptyDeckPlayer.refillDeck()
+            refilledPlayer.deck.length should be(1)
+            refilledPlayer.discard.length should be(0)
+
+            val drawFromEmptyPlayer = emptyDeckPlayer.drawCardFromDeck()
+            drawFromEmptyPlayer.deck.length should be(0)
+            drawFromEmptyPlayer.discard.length should be(0)
+        }
+
+        "should have a toString() that prints out the deck, hand and discard" in {
+            player.toString() should fullyMatch regex regx
+            player.deckToString() should fullyMatch regex regx
+            player.handToString() should fullyMatch regex regx
+            player.discardToString() should fullyMatch regex regx
+        }
+        "should be able to draw 5 Cards" in {
+            val fivedrawn = player.drawFiveCardsFromDeck()
+            fivedrawn.hand.length should be(5)
+            fivedrawn.deck.length should be(5)
+        }
+        "not be able to refill deck if it's not empty" in {
+            val refilled = player.refillDeck()
+            refilled == player should be(true)
+        }
+        "discard all Cards from hand" in {
+            val fiveDrawn = player.drawFiveCardsFromDeck()
+            val discarded = fiveDrawn.discardAllFromHand()
+
+            discarded.deck.length shouldBe(5)
+            discarded.hand.length shouldBe(0)
+            discarded.discard.length shouldBe(5)
+        }
+        "count the money currently in Hand" in {
+            val fiveDrawn = player.drawFiveCardsFromDeck()
+            fiveDrawn.getMoneyInHand() shouldBe(5)
+
+            val drawOnce = player.drawCardFromDeck()
+            drawOnce.getMoneyInHand() shouldBe(1)
+        }
+
+        "purchase a card from Stock" in {
+            val p1 = player.drawFiveCardsFromDeck()
+            val result = p1.purchaseCard(Card.Silber, stock)
+
+            result.discard.contains(Card.Silber) should be(true)
+            result.hand.contains(Card.Silber) should be(false)
+            result.deck.contains(Card.Silber) should be(false)
+        }
+
+        "not purchase a card that is not in Stock" in {
+            val p1 = player.drawFiveCardsFromDeck()
+            val result = p1.purchaseCard(Card.Werkstatt, stock)
+
+            result.discard.contains(Card.Werkstatt) should be(false)
+            result.hand.contains(Card.Werkstatt) should be(false)
+            result.deck.contains(Card.Werkstatt) should be(false)
+        }
+    }
+}
